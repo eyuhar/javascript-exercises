09eecf9610eda24fa7b577992dac982a26557302
Eyyuep <eyuep.harputlu@gmail.com>
2022-09-08 11:32:29 +0200

    add solution changes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,6 @@
-const removeFromArray = function(array, removal) {
-    let args = Array.from(arguments);
-    args.shift();
-
-    args.forEach(element => {
-        if(array.includes(element)){
-            while (array.indexOf(element) !== -1) {
-                array.splice(array.indexOf(element), 1);
-            }
-        }
-    });
-    return array;
+const removeFromArray = function(...args) {
+    const arr = args[0];
+    return arr.filter(val => !args.includes(val));
 };
 
 // Do not edit below this line
