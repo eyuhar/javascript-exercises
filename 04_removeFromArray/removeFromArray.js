c28ab27dbf6ac05773071ff483dd886d5f754160
Eyyuep <eyuep.harputlu@gmail.com>
2022-09-08 10:18:14 +0200

    add solutions to exercises 03 and 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removal) {
+    let args = Array.from(arguments);
+    args.shift();
 
+    args.forEach(element => {
+        if(array.includes(element)){
+            while (array.indexOf(element) !== -1) {
+                array.splice(array.indexOf(element), 1);
+            }
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
